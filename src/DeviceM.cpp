d0bc6f485efe3ec5243d300c6218c08e904d3995
GiovaBordi <bordignon.giovanni.04@gmail.com>
2024-12-30 16:43:33 +0100

    Update DeviceM.cpp
    Pulizia e rettifica costruttori

--- src/DeviceM.cpp
+++ src/DeviceM.cpp
@@ -5,14 +5,16 @@
 
 #include "DeviceM.h"
 
-DeviceM::DeviceM() {
+/*DeviceM::DeviceM() {
 	name = DEFAULT_NAME;
 	id = ID_Counter++;
 	status = DEFAULT_STATUS;
 	energy = DEFAULT_ENERGY;
 	totalEnergy = DEFAULT_ENERGY;	// Uso la medesima costante di energy anche per totalEnergy
-	lastOn = onTime = offTime = my_clock::Clock(24, 0);
-}
+	lastOn.set24();
+	onTime.set24();
+    	offTime.set24();
+}*/
 
 DeviceM::DeviceM(std::string name, double energy) {
 	this->name = name;
@@ -20,15 +22,17 @@ DeviceM::DeviceM(std::string name, double energy) {
 	status = DEFAULT_STATUS;
 	this->energy = energy;
 	totalEnergy = DEFAULT_ENERGY;	// Uso la medesima costante di energy anche per totalEnergy
-	lastOn = onTime = offTime = my_clock::Clock(24, 0);
+	lastOn.set24();
+	onTime.set24();
+    	offTime.set24();
 }
 
 void DeviceM::set_offTime(int h, int m) {
 	offTime = my_clock::Clock(h, m);
 }
 
-void DeviceM::set_offTime(const my_clock::Clock& t1) {
-	offTime = t1;
+void DeviceM::set_offTime(const my_clock::Clock& t) {
+	offTime = t;
 }
 
 my_clock::Clock DeviceM::get_offTime() const {
