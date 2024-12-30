6ff1716fb96e3793919689fba74df0b775c13052
GiovaBordi <bordignon.giovanni.04@gmail.com>
2024-12-31 00:07:52 +0100

    Update DeviceM.h

--- include/DeviceM.h
+++ include/DeviceM.h
@@ -10,18 +10,24 @@
 
 class DeviceM : public Device {
 	private:
-		static constexpr char DEFAULT_NAME[] = "DispositivoM";
-		my_clock::Clock offTime;
+		// Nulla
 	public:
-		// Costruttori
-		//DeviceM();
-		DeviceM(std::string name = DEFAULT_NAME, double energy = DEFAULT_ENERGY); // string --> name, double --> energy
+		// Costruttore
+		DeviceM(std::string name, double energy); // string --> name, double --> energy
 		
-		// Funzioni per il settaggio dell'ora di spegnimento
-		void set_offTime(int, int);
+		// Funzione per l'accensione del Dispositivo
+		void turnOn(const my_clock::Clock &) override;
+		
+		// Funzione per il settaggio dell'ora di accensione del Dispositivo
+		void set_onTime(const my_clock::Clock &) override;
+		
+		// Funzione per il settaggio dell'ora di spegnimento
 		void set_offTime(const my_clock::Clock&);
 
-		my_clock::Clock get_offTime() const;
+		// Funzione per ottenere una stringa contenente i dati del Dispositivo
+		std::string toString() const override;
 };
+
+std::ostream &operator<<(std::ostream &, const DeviceM &);
 
 #endif // DEVICEM_H
