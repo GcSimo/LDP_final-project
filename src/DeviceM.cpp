20e1a3aae36f6c689e2ae4093e939d599619b48e
GiovaBordi <bordignon.giovanni.04@gmail.com>
2024-12-31 00:23:43 +0100

    Update DeviceM.cpp

--- src/DeviceM.cpp
+++ src/DeviceM.cpp
@@ -5,36 +5,43 @@
 
 #include "DeviceM.h"
 
-/*DeviceM::DeviceM() {
-	name = DEFAULT_NAME;
-	id = ID_Counter++;
-	status = DEFAULT_STATUS;
-	energy = DEFAULT_ENERGY;
-	totalEnergy = DEFAULT_ENERGY;	// Uso la medesima costante di energy anche per totalEnergy
-	lastOn.set24();
-	onTime.set24();
-    	offTime.set24();
-}*/
-
 DeviceM::DeviceM(std::string name, double energy) {
 	this->name = name;
 	id = ID_Counter++;
 	status = DEFAULT_STATUS;
 	this->energy = energy;
-	totalEnergy = DEFAULT_ENERGY;	// Uso la medesima costante di energy anche per totalEnergy
+	totalEnergy = DEFAULT_TOTALT_ENERGY;
 	lastOn.set24();
 	onTime.set24();
-    	offTime.set24();
+    offTime.set24();
 }
 
-void DeviceM::set_offTime(int h, int m) {
-	offTime = my_clock::Clock(h, m);
+void DeviceM::turnOn(const my_clock::Clock & t) {
+	// se prima il dispositivo prima era spento, aggiorno l'orario dell'ultima accensione
+	if (!status)
+		lastOn = t;
+	status = 1;
 }
 
-void DeviceM::set_offTime(const my_clock::Clock& t) {
-	offTime = t;
+void DeviceM::set_onTime(const my_clock::Clock & onTime) {
+    this->onTime = onTime;
 }
 
-my_clock::Clock DeviceM::get_offTime() const {
-	return offTime;
+void DeviceM::set_offTime(const my_clock::Clock& offTime) {
+	this->offTime = offTime;
+}
+
+std::string DeviceM::toString() const {
+	std::string str = "Dispositivo: " + name + "\nID: " + std::to_string(id) + "\nStato attuale: Dispositivo ";
+	if (status) str += "acceso\n";
+	else str += "spento\n";
+	if (energy > 0) str += "Produzione: " + std::to_string(energy) + "kW\nTot. produzione: " + std::to_string(totalEnergy) + " kWh\n";
+	else str += "Consumo: " + std::to_string(energy) + "kW\nTot. consumo: " + std::to_string(totalEnergy) + " kWh\n";
+	str += "Ultima accensione: " + lastOn.toString() + "\nAccensione programmata: " + onTime.toString() + "\nSpegnimento programmato: " + offTime.toString() + "\n";
+	return str;
+	// Togliere ultima accensione ore 24:00
+}
+
+std::ostream &operator<<(std::ostream &os, const DeviceM &t) {
+		return os << t.toString();
 }
