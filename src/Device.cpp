e8d2889b5295e69083d420ecb600ee09cb54e078
GiovaBordi <bordignon.giovanni.04@gmail.com>
2024-12-31 11:22:26 +0100

    Update Device.cpp
    Aggiunto namespace

--- src/Device.cpp
+++ src/Device.cpp
@@ -5,56 +5,59 @@
 
 #include "Device.h"
 
-int Device::ID_Counter = 0;
+namespace device {
+	int Device::ID_Counter = 0;
 
-void Device::turnOff(const my_clock::Clock &t) {
-	// se il dispositivo prima era acceso, aggiorno il consumo complessivo
-	if (status)
-		totalEnergy += (t - lastOn).toSeconds() * energy;
-    status = 0;
-}
+	void Device::turnOff(const my_clock::Clock &t) {
+		// se il dispositivo prima era acceso, aggiorno il consumo complessivo
+		if (status)
+			totalEnergy += (t - lastOn).toSeconds() * energy;
+    	status = 0;
+	}
 
-void Device::changeStatus(const my_clock::Clock &t) {
-	if (status)
-		this->turnOff(t);
-	else
-		this->turnOn(t);
-}
+	void Device::changeStatus(const my_clock::Clock &t) {
+		if (status)
+			this->turnOff(t);
+		else
+			this->turnOn(t);
+	}
 
-void Device::refreshDevice(const my_clock::Clock &t) {
-	if (t >= onTime)
-		this->turnOn(onTime);
-	if (t >= offTime)
-		this->turnOff(offTime);
-}
+	void Device::refreshDevice(const my_clock::Clock &t) {
+		if (t >= onTime)
+			this->turnOn(onTime);
+		if (t >= offTime)
+			this->turnOff(offTime);
+	}
 
-std::string Device::get_name() const {
-    return name;
-}
+	std::string Device::get_name() const {
+    		return name;
+	}
 
-int Device::get_id() const {
-    return id;
-}
+	int Device::get_id() const {
+    		return id;
+	}
 
-bool Device::get_status() const {
-    return status;
-}
+	bool Device::get_status() const {
+    		return status;
+	}
 
-double Device::get_energy() const {
-    return energy;
-}
+	double Device::get_energy() const {
+    		return energy;
+	}
 
-my_clock::Clock Device::get_onTime() const {
-    return onTime;
-}
+	my_clock::Clock Device::get_onTime() const {
+    		return onTime;
+	}
 
-my_clock::Clock Device::get_offTime() const {
-    return offTime;
-}
+	my_clock::Clock Device::get_offTime() const {
+    		return offTime;
+	}
 
-double Device::get_totalEnergy(const my_clock::Clock &t) {
-	// energia consumata nelle vecchie accensioni + quella consumata dall'ultima accensione
-	// NB: se il dispositivo è spento, l'energia consumata dall'ultima accensione è 0 perché
-	//     moltiplico per status che è per l'appunto 0
-	return totalEnergy + (t - lastOn).toSeconds() * energy * status;
+	double Device::get_totalEnergy(const my_clock::Clock &t) {
+		// energia consumata nelle vecchie accensioni + quella consumata dall'ultima accensione
+		// NB: se il dispositivo è spento, l'energia consumata dall'ultima accensione è 0 perché
+		//     moltiplico per status che è per l'appunto 0
+		return totalEnergy + (t - lastOn).toSeconds() * energy * status;
+	}
+
 }
