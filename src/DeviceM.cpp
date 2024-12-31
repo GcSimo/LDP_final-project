ec7b81910307478b14304826188cb6e2fc07fbcd
GiovaBordi <bordignon.giovanni.04@gmail.com>
2024-12-31 11:45:35 +0100

    Update DeviceM.cpp
    Aggiunto namespace
    Correzione toString()

--- src/DeviceM.cpp
+++ src/DeviceM.cpp
@@ -5,43 +5,49 @@
 
 #include "DeviceM.h"
 
-DeviceM::DeviceM(std::string name, double energy) {
-	this->name = name;
-	id = ID_Counter++;
-	status = DEFAULT_STATUS;
-	this->energy = energy;
-	totalEnergy = DEFAULT_TOTALT_ENERGY;
-	lastOn.set24();
-	onTime.set24();
-    offTime.set24();
-}
+namespace device {
+	DeviceM::DeviceM(std::string name, double energy) {
+		this->name = name;
+		id = ID_Counter++;
+		status = DEFAULT_STATUS;
+		this->energy = energy;
+		totalEnergy = DEFAULT_TOTALT_ENERGY;
+		lastOn.set24();
+		onTime.set24();
+    		offTime.set24();
+	}
 
-void DeviceM::turnOn(const my_clock::Clock & t) {
-	// se prima il dispositivo prima era spento, aggiorno l'orario dell'ultima accensione
-	if (!status)
-		lastOn = t;
-	status = 1;
-}
+	void DeviceM::turnOn(const my_clock::Clock & t) {
+		// se prima il dispositivo prima era spento, aggiorno l'orario dell'ultima accensione
+		if (!status)
+			lastOn = t;
+		status = 1;
+	}
 
-void DeviceM::set_onTime(const my_clock::Clock & onTime) {
-    this->onTime = onTime;
-}
+	void DeviceM::set_onTime(const my_clock::Clock & onTime) {
+   		this->onTime = onTime;
+	}
 
-void DeviceM::set_offTime(const my_clock::Clock& offTime) {
-	this->offTime = offTime;
-}
+	void DeviceM::set_offTime(const my_clock::Clock& offTime) {
+		this->offTime = offTime;
+	}
 
-std::string DeviceM::toString() const {
-	std::string str = "Dispositivo: " + name + "\nID: " + std::to_string(id) + "\nStato attuale: Dispositivo ";
-	if (status) str += "acceso\n";
-	else str += "spento\n";
-	if (energy > 0) str += "Produzione: " + std::to_string(energy) + "kW\nTot. produzione: " + std::to_string(totalEnergy) + " kWh\n";
-	else str += "Consumo: " + std::to_string(energy) + "kW\nTot. consumo: " + std::to_string(totalEnergy) + " kWh\n";
-	str += "Ultima accensione: " + lastOn.toString() + "\nAccensione programmata: " + onTime.toString() + "\nSpegnimento programmato: " + offTime.toString() + "\n";
-	return str;
-	// Togliere ultima accensione ore 24:00
-}
+	std::string DeviceM::toString() const {
+		std::string str = "Dispositivo: " + name + "\nID: " + std::to_string(id) + "\nStato attuale: Dispositivo ";
+		str += status ? "acceso\n" : "spento\n";
+		if (energy > 0) str += "Produzione: " + std::to_string(energy) + "kW\nTot. produzione: " + std::to_string(totalEnergy) + " kWh\n";
+		else str += "Consumo: " + std::to_string(energy) + "kW\nTot. consumo: " + std::to_string(totalEnergy) + " kWh\nUltima accensione: ";
+		str +=  (lastOn.isValid()) ? lastOn.toString() : "Nessuna accensione avvenuta";
+		str += "\nAccensione programmata: ";
+		str += (onTime.isValid()) ? onTime.toString() : "Accensione non programmata";
+		str += "\nSpegnimento programmato: ";
+		str += (offTime.isValid()) ? offTime.toString() : "Spegnimento non programmato";
+		str += "\n";
+		return str;
+	}
 
-std::ostream &operator<<(std::ostream &os, const DeviceM &t) {
-		return os << t.toString();
+	std::ostream &operator<<(std::ostream &os, const DeviceM &t) {
+			return os << t.toString();
+	}
+
 }
