85e97395c8ae4cc6aa37c661668227b0b8fc93a2
GiovaBordi <bordignon.giovanni.04@gmail.com>
2024-12-31 11:19:27 +0100

    Update DeviceM.h
    Aggiunto namespace

--- include/DeviceM.h
+++ include/DeviceM.h
@@ -8,26 +8,28 @@
 
 #include "Device.h"
 
-class DeviceM : public Device {
-	private:
-		// Nulla
-	public:
-		// Costruttore
-		DeviceM(std::string name, double energy); // string --> name, double --> energy
+namespace device {
+	class DeviceM : public Device {
+		private:
+			// Nulla
+		public:
+			// Costruttore
+			DeviceM(std::string name, double energy); // string --> name, double --> energy
 		
-		// Funzione per l'accensione del Dispositivo
-		void turnOn(const my_clock::Clock &) override;
+			// Funzione per l'accensione del Dispositivo
+			void turnOn(const my_clock::Clock &) override;
 		
-		// Funzione per il settaggio dell'ora di accensione del Dispositivo
-		void set_onTime(const my_clock::Clock &) override;
+			// Funzione per il settaggio dell'ora di accensione del Dispositivo
+			void set_onTime(const my_clock::Clock &) override;
 		
-		// Funzione per il settaggio dell'ora di spegnimento
-		void set_offTime(const my_clock::Clock&);
+			// Funzione per il settaggio dell'ora di spegnimento
+			void set_offTime(const my_clock::Clock&);
 
-		// Funzione per ottenere una stringa contenente i dati del Dispositivo
-		std::string toString() const override;
-};
+			// Funzione per ottenere una stringa contenente i dati del Dispositivo
+			std::string toString() const override;
+	};
 
-std::ostream &operator<<(std::ostream &, const DeviceM &);
+	std::ostream &operator<<(std::ostream &, const DeviceM &);
+}
 
 #endif // DEVICEM_H
