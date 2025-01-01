754972819099d41e156a2c2e5d7d1833ea266b7c
Giacomo Simonetto <simonettogiacomo04@gmail.com>
2025-01-01 23:16:51 +0100

    Add test_home file

---
+++ test/test_home.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "DeviceM.h"
+#include "DeviceCP.h"
+#include "Home.h"
+using namespace std;
+
+int main () {
+	cout << "Programma Robotic-Home del gruppo ~BankAccount()" << endl << endl;
+	
+	// creazione casa
+	Home h;
+
+	vector<string> inputs = {
+		"show",
+		"set Impianto_fotovoltaico 6:00 12:00",
+		"set Pompa_di_calore_+_termostato 1:20 7:20",
+		"set time 6:00",
+		"show",
+		"set Televisore 10:00",
+		"set time 12:00",
+		"set Lavatrice 12:30",
+		"set Lavastoviglie 12:31",
+		"set Televisore 12:32",
+		"set time 13:00",
+		"set Televisore on",
+		"set time 14:30",
+		//"set time 300",
+		//"set Frigorifero on",
+		//"set time 600",
+		//"set Frigorifero off",
+		//"set time 660",
+		//"set Frigorifero on",
+		//"set time 1260",
+		//"set Frigorifero off",
+		//"set Televisore 1300",
+		//"set time 1400",
+		"show"
+	};
+
+	/* while(true){
+		std::string str;
+		std::getline(std::cin,str);
+		h.listen(str);
+	} */
+
+	for(string s : inputs) {
+		h.listen(s);
+	}
+}
