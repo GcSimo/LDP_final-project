cc3ab59c88dad4458a786c8c6c1cb25763b77ac7
Giovanni Bordignon <bordignon.giovanni.04@gmail.com>
2025-01-13 00:37:43 +0100

    Modifica funzione e pulizia
    1. Modifica funzione turnOff;
    2. Pulizia delle funzioni rese inline.

--- src/Device.cpp
+++ src/Device.cpp
@@ -8,15 +8,13 @@
 namespace domotic_home {
 	int Device::ID_Counter = 0;
 
-	bool Device::turnOff(const Clock &t) {
+	void Device::turnOff(const Clock &t) {
 		// se il dispositivo prima era acceso, aggiorno il consumo complessivo e l'orario dell'ultimo aggiornamento del consumo
 		if (status) {
 			totalEnergy += (t - lastEnergyUpdate).toHours() * energy;
 			lastEnergyUpdate = t;
-			status = 0;
-			return true;
 		}
-		return false;
+		status = 0;
 	}
 
 	void Device::refreshDevice(const Clock &t) {
@@ -36,34 +34,6 @@ namespace domotic_home {
 	void Device::resetTotalEnergy() {
 		totalEnergy = DEFAULT_TOTALT_ENERGY;
 		lastEnergyUpdate.setInvalid();
-	}
-
-	std::string Device::get_name() const {
-		return name;
-	}
-
-	int Device::get_id() const {
-		return id;
-	}
-
-	bool Device::get_status() const {
-		return status;
-	}
-
-	double Device::get_energy() const {
-		return energy;
-	}
-
-	double Device::get_totalEnergy() const {
-		return totalEnergy;
-	}
-
-	Clock Device::get_onTime() const {
-		return onTime;
-	}
-
-	Clock Device::get_offTime() const {
-		return offTime;
 	}
 
 	std::string Device::toString() const {
