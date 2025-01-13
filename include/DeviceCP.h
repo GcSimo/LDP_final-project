3f8e3e2f121fb1ac5dcdeaadb1513f6720e7ec82
Giovanni Bordignon <bordignon.giovanni.04@gmail.com>
2025-01-13 17:34:19 +0100

    Commenti

--- include/DeviceCP.h
+++ include/DeviceCP.h
@@ -1,6 +1,14 @@
 /*
 	FILE HEADER DEVICECP.H
 	Autore:     Giovanni Bordignon
+
+	La sottoclasse DeviceCP consente l'uso e la gestione di dispositivi a
+	ciclo prefissato, dove la durata di funzionamento di un dato dispositivo
+	risulta essere già prefissata. Le funzioni "turnOn(...)" e "set_turnOn(...)",
+	agiscono di conseguenza, impostando automaticamente l'orario di spegnimento
+	in funzione del ciclo prefissato di funzionamento. È inoltre presente la
+	funzione "get_cycle()" la quale permette di ottenere il ciclo stesso di
+	funzionamento, nella forma di oggetto della classe di Clock.
 */
 
 #ifndef DEVICECP_H
@@ -11,15 +19,17 @@
 namespace domotic_home {
 	class DeviceCP : public Device {
 		private:
-			Clock cycle;
+			// Variabile membro
+			Clock cycle;	// Variabile per il salvataggio della durata del ciclo di funzionamento
 		public:
 			// Costruttore
+			// Costruttore con nome (string), consumo / produzione energetica istantanea (double) e ciclo di funzionamento (Clock) come parametri
 			DeviceCP(std::string, double, const Clock &);
 
-			// Funzione per l'accensione del Dispositivo
+			// Funzione deputata all'accensione del Dispositivo
 			void turnOn(const Clock &) override;
 
-			// Funzione per il settaggio dell'ora di accensione del Dispositivo
+			// Funzione atta al settaggio dell'orario di accensione del dispositivo
 			void set_onTime(const Clock &) override;
 
 			// Funzione per ottenere il ciclo di funzionamento del dispositivo
