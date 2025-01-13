31ff6def1bf859cbabf5b3f7d59e51e131fb8cb3
Giovanni Bordignon <bordignon.giovanni.04@gmail.com>
2025-01-13 17:40:14 +0100

    Commenti

--- src/DeviceM.cpp
+++ src/DeviceM.cpp
@@ -1,11 +1,19 @@
 /*
-	FILE HEADER DEVICEM.CPP
+	FILE SORGENTE DEVICEM.CPP
 	Autore:     Giovanni Bordignon
+
+	Implementazione delle funzioni della sottoclasse DeviceM. 
 */
 
 #include "DeviceM.h"
 
 namespace domotic_home {
+	/**
+	 * @brief Costruttore della sottoclasse DeviceM con parametri stringa (--> nome) e double (--> energia)
+	 * 
+	 * @param name nome del dispositivo
+	 * @param energy consumo istantaneo del dispositivo
+	 */
 	DeviceM::DeviceM(std::string name, double energy) {
 		this->name = name;
 		id = ID_Counter++;
@@ -17,6 +25,11 @@ namespace domotic_home {
 		lastEnergyUpdate.setInvalid();
 	}
 
+	/**
+	 * @brief Funzione per l'accensione del dispositivo
+	 * 
+	 * @param t orario di accensione del dispositivo
+	 */
 	void DeviceM::turnOn(const Clock & t) {
 		// se prima il dispositivo prima era spento, aggiorno l'orario dell'ultimo aggiornamento del consumo
 		// infatti se prima era spento, il consumo non è cambiato dall'ultima volta in cui è stato aggiornato
@@ -25,11 +38,21 @@ namespace domotic_home {
 		status = 1;
 	}
 
+	/**
+	 * @brief Funzione per il settaggio dell'orario di accensione del dispositivo
+	 * 
+	 * @param onTime orario di accensione del dispositivo
+	 */
 	void DeviceM::set_onTime(const Clock & onTime) {
    		this->onTime = onTime;
 	}
 
-	void DeviceM::set_offTime(const Clock& offTime) {
+	/**
+	 * @brief Funzione per il settaggio dell'orario di spegnimento del dispositivo
+	 * 
+	 * @param offTime orario di spegnimento del dispositivo
+	 */
+	void DeviceM::set_offTime(const Clock & offTime) {
 		this->offTime = offTime;
 	}
 }
