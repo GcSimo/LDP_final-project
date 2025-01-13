955d4a650e47ec23231270c25a7b2a9f5cef630c
Giovanni Bordignon <bordignon.giovanni.04@gmail.com>
2025-01-13 17:35:05 +0100

    Commenti

--- include/DeviceM.h
+++ include/DeviceM.h
@@ -1,6 +1,13 @@
 /*
 	FILE HEADER DEVICEM.H
 	Autore:     Giovanni Bordignon
+
+	La sottoclasse DeviceM permette l'uso di dispositivi manuali, ove dunque è
+	possibile programmare un orario di accensione e un orario di spegnimento,
+	senza alcun vincolo di funzionamento a ciclo prefissato.
+	Nella sottoclasse viene eseguito l'override delle funzioni "turnOn(...)" e
+	"set_onTime(...)" e viene resa disponibile la funzione "set_offTime(...)",
+	indispensabile per l'uso con i dispositivi manuali.
 */
 
 #ifndef DEVICEM_H
@@ -10,20 +17,18 @@
 
 namespace domotic_home {
 	class DeviceM : public Device {
-		private:
-			// Nulla
 		public:
 			// Costruttore
-			DeviceM(std::string, double); // string --> name, double --> energy
+			DeviceM(std::string, double); // Costruttore con nome (string) e consumo / produzione energetica istantanea (double) come parametri
 		
-			// Funzione per l'accensione del Dispositivo
+			// Funzione deputata all'accensione del Dispositivo
 			void turnOn(const Clock &) override;
 		
-			// Funzione per il settaggio dell'ora di accensione del Dispositivo
+			// Funzione atta al settaggio dell'orario di accensione del dispositivo
 			void set_onTime(const Clock &) override;
 		
-			// Funzione per il settaggio dell'ora di spegnimento
-			void set_offTime(const Clock&);
+			// Funzione atta al settaggio dell'orario di spegnimento del dispositivo
+			void set_offTime(const Clock &);
 	};
 }
 #endif // DEVICEM_H
